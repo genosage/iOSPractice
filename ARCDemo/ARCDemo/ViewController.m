425cc5aefa4a1dc67554221fc0a98bbba544af30
Tian Zhang <tianz2@student.unimelb.edu.au>
2017-07-26 15:02:23 +0800

    ARC first commit

--- ARCDemo/ARCDemo/ViewController.m
+++ ARCDemo/ARCDemo/ViewController.m
@@ -2,12 +2,28 @@
 
 @interface ViewController ()
 
+@property (weak) NSString *name;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.name = [[NSString alloc] init];
+    
+    /*
+    NSMutableString *string = [NSMutableString stringWithFormat:@"Josh"];
+    
+    self.name = string;
+    
+    [string appendString:@"ua"];
+    
+    NSLog(@"%@", self.name);
+    
+    NSLog(@"%@", string);
+    */
     
 }
 
