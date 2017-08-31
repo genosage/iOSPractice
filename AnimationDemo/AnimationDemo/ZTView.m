1d1bdc8f83f6c20422bd43abd12479e61b3a5ffd
Tian Zhang <tianz2@student.unimelb.edu.au>
2017-09-01 01:50:42 +0800

    ~

--- AnimationDemo/AnimationDemo/ZTView.m
+++ AnimationDemo/AnimationDemo/ZTView.m
@@ -2,7 +2,28 @@
 
 @implementation ZTView
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [super touchesBegan:touches withEvent:event];
+    
+    NSTimeInterval dur = 0.2;
+    UIViewAnimationOptions options = UIViewAnimationOptionCurveEaseIn;
+    
+    [UIView animateWithDuration:dur delay:0 options:options animations:^
+     {
+         self.transform = CGAffineTransformMakeScale(1.3f, 1.3f);
+     } completion:^(BOOL finished)
+     {
+         [UIView animateWithDuration:dur delay:0 options:options animations:^
+          {
+              self.transform = CGAffineTransformMakeScale(1.0f, 1.0f);
+          } completion:nil];
+     }];
+}
+
+
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [super touchesMoved:touches withEvent:event];
+    
     UITouch *touch = [touches anyObject];
     
     CGPoint current = [touch locationInView:self];
---
+++ RunloopDemo/RunloopDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunloopDemo
+//
+//  Created by 张添 on 2017/8/31.
+//  Copyright © 2017年 Tian Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
